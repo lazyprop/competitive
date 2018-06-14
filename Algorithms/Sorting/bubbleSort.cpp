b23e3d899aeab4e8289918b0dfb5850bbe3dcf2e
datboiee <ishaan56verma@gmail.com>
2018-06-14 08:22:09 +0530

    bubble sort

--- Algorithms/Sorting/bubbleSort.cpp
+++ Algorithms/Sorting/bubbleSort.cpp
@@ -9,4 +9,29 @@ int main()
 	int a[n];
 	
 	for (int i = 0;i<n;i++)
+	{
+		cin>>a[i];
+		
+	}
+	
+	// sort
+	
+	for (int i = 0;i<n;i++)
+	{
+		for (int j= 0;j<n-1;j++)
+		{
+			if (a[j] > a[j+1])
+			{
+				int temp = a[j];
+				a[j] = a[j+1];
+				a[j+1] = temp;
+			}
+		}
+	}
+	cout<<endl;
+	
+	for (int i = 0;i<n;i++)
+	{
+		cout<<a[i]<<" ";
+	}
 }
