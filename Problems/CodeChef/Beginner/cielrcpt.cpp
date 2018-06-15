d8e7a3e1bee6bfedbb7b2f3280da6f1d34e28d23
datboiee <ishaan56verma@gmail.com>
2018-06-15 22:48:19 +0530

    luckfour

---
+++ Problems/CodeChef/Beginner/cielrcpt.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+	int t;
+	cin>>t;
+
+	for (int j = 0;j<t;t++)
+	{
+		int p;
+		cin>>p;
+		int count = 0;
+		
+		while (p)
+		{
+
+			int pwr;
+			for (int i = 0;i<11;i++)
+			{
+				if (int(pow(2,i) > p))
+				{
+					pwr = i-1;
+					break;
+				}
+			}
+			p = p- int(pow(2,pwr));
+			count++;
+		}
+
+		cout<<count;
+	}
+}
+
+
---
+++ Problems/CodeChef/Beginner/luckfour.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+int strlen(string n)
+{
+	int i = 0;
+	while (true)
+	{
+		if (n[i] == '\0')
+		{
+			break;
+			
+		}
+		i++;
+	}
+	return i;
+}
+int main ()
+{
+	int t;
+	cin>>t;
+
+	for (int i = 0;i<t;i++)
+	{
+		string n;
+		cin>>n;
+
+		int count = 0;
+		
+		for (int i = 0;i<strlen(n);i++)
+		{
+			if ( n[i] == 52)
+			{
+				count++;
+			}
+		}
+
+		cout<<count<<endl;
+	}
+}
+
