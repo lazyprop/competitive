e61db3f4da1f634067a3724729a6f1ba35baa422
datboiee <ishaan56verma@gmail.com>
2018-11-19 19:36:46 +0530

    Ilya and Queries | Codeforces

--- Problems/Codeforces/313B.cpp
+++ Problems/Codeforces/313B.cpp
@@ -17,17 +17,16 @@ int main()
 	}
 	int sum[n];
 	memset(sum,0,sizeof(sum));
-	sum[0] = dp[0];
+	sum[0] = 0;
 	for (int i = 1; i<n; i++)
 	{
-		sum[i] = sum[i-1] + 1;
+		sum[i] = sum[i-1] + dp[i-1];
 	}
-	
 	int m; cin>>m;
 	for (int i = 0; i<m; i++)
 	{
 		int a,b; cin>>a>>b;
-		//cout<<sum[b]<<" "<<sum[a]<<endl;
+		//cout<<sum[b-1]<<" "<<sum[a-1]<<endl;
 		cout<<sum[b-1]-sum[a-1]<<endl;
 	}
 
