ea5c87ffafccd2259cef942cb5b913d124616772
n1trob4ct3r <ishaan56verma@gmail.com>
2019-09-04 08:35:18 +0530

    zco chewing ez pz

--- Problems/CodeChef/ZCOPRAC/chewing.cpp
+++ Problems/CodeChef/ZCOPRAC/chewing.cpp
@@ -1,36 +1,46 @@
 #include<iostream>
-#include<algorithm>
 #include<vector>
+#include<limits>
+#include<math.h>
+#include<bits/stdc++.h>
+
 using namespace std;
+
+#define make_graph(m,G) for (int i = 0; i<m; i++) { int a,b; cin>>a>>b;G[a-1].push_back(b-1); G[b-1].push_back(a-1);}
+#define make_dir_graph(m,G) for (int i = 0; i<m; i++) { int a,b; cin>>a>>b;G[a-1].push_back(b-1);}
+#define make_weighted_graph(m,G,w) for (int i = 0; i<m; i++) { int a,b; cin>>a>>b; cin>>w[a-1][b-1];w[b-1][a-1]=w[a-1][b-1];G[a-1].push_back(b-1); G[b-1].push_back(a-1);}
+#define make_weighted_dir_graph(m,G,w) for (int i = 0; i<m; i++) { int a,b; cin>>a>>b; cin>>w[a-1][b-1]; G[a-1].push_back(b-1);}
 
 int main()
 {
-	int n,k; cin>>n>>k;
-	vector <int> v;
-	for (int i = 0;i<n;i++)
+	int t=1;
+	// cin>>t;
+	while (t--)
 	{
-		int tmp;
-		cin>>tmp;
-
-		if (tmp<k)
+		// code
+		int n,k; cin>>n>>k;
+		int a[n];
+		for (int i = 0; i<n; i++)
 		{
-			v.push_back(tmp);
+			cin>>a[i];
 		}
-	}
-	
-	long long ans=0;
-	for (int i = 0;i<v.size();i++)
-	{
-		for (int j = i+1;j<v.size();j++)
+
+		sort(a,a+n);
+		long long count=0;
+		int i = 0, j = n-1;
+		while (i < j)
 		{
-			if (v[i] + v[j] <k)
+			for (j = j; j>i; j--)
 			{
-				ans++;
+				if (a[i] + a[j] < k)
+				{
+					break;
+				}
 			}
+			count+=j-i;
+			i++;
 		}
+		cout<<count;
+		cout<<endl;	
 	}
-	cout<<ans<<endl;
 }
-
-
-
