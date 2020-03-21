ac12aee2159d56dd03821d93b730933e1184b7d7
Ishaan Verma <ishaan56verma@gmail.com>
2020-03-21 17:10:32 +0530

    more renaming

---
+++ cf/1325/B.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int T; cin>>T;
+    while (T--)
+    {
+        int n; cin>>n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin>>a[i];
+        }
+    }
+}
+
