63d4a520488c5ab13fe0e8f7144af6ff5ee5d714
Ishaan Verma <n1trob4ct3r@protonmail.com>
2020-06-20 02:14:07 +0530

    removed unnecessary merge function

--- algos/mergesort/mergesort.cpp
+++ algos/mergesort/mergesort.cpp
@@ -1,32 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void merge(int a[], int l, int m, int r)
-{
-    int merged[m - l + 1] = {};
-
-    int i = l, j = m+1, k = 0;
-    while (i <= m and j <= r)
-    {
-        if (a[i] <= a[j])
-        {
-            merged[k] = a[i];
-            i++;
-        }
-        else
-        {
-            merged[k] = a[j];
-            j++;
-        }
-        k++;
-    }
-
-    for (k = 0; k < m - l + 1; k++)
-    {
-        a[k+l] = merged[k];
-    }
-}
-
 void mergeSort(int a[], int l, int r)
 {
     if (l < r)
